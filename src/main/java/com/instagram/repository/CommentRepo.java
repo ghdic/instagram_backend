245871dcfcb4b06c05411cc66d900c66f6b7c19f
ghdic <ghdic77@gmail.com>
2021-10-03 12:58:01 +0900

    delete부분 @Transactional 추가

--- src/main/java/com/instagram/repository/CommentRepo.java
+++ src/main/java/com/instagram/repository/CommentRepo.java
@@ -3,6 +3,7 @@ package com.instagram.repository;
 import com.instagram.model.Comments;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
@@ -12,5 +13,6 @@ public interface CommentRepo extends JpaRepository<Comments, Integer> {
     Comments save(Comments comments);
     Comments findCommentsByCommentId(int commentId);
     ArrayList<Comments> findAllByPostId(int postId);
+    @Transactional
     void deleteCommentsByCommentId(int commentId);
 }
--- src/main/java/com/instagram/repository/PostRepo.java
+++ src/main/java/com/instagram/repository/PostRepo.java
@@ -4,6 +4,7 @@ import com.instagram.model.Post;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
@@ -13,5 +14,6 @@ public interface PostRepo extends JpaRepository<Post, Integer> {
     Post findPostByPostId(int postId);
     ArrayList<Post> findAll(Sort sort);
     ArrayList<Post> findPostsByUser_Uid(Sort sort, String user_uid);
+    @Transactional
     void deleteByPostId(int postId);
 }
